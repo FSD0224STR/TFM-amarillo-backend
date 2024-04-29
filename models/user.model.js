e75f7706ca30508fdb7d00d7239a0709f0b4d190
oriolorriols <oriol.orriols@gmail.com>
2024-04-29 20:09:03 +0200

    UsersCreate Default Users
    Añadida la funcionalidad para que haciendo un get a /user/resetToDefault se borren y se creen todas las entradas y se restablezcan a las de por defecto.

--- models/user.model.js
+++ models/user.model.js
@@ -2,24 +2,93 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    id: {
-      type: String,
-      required: true, 
-      },
-    name:{
+  email: {
       type: String,
       required: true
-      },
-    surname: {
+  },
+  password: {
       type: String,
       required: true
-      },
-    password:{
+  },
+  profileType: {
       type: String,
       required: true
-      }   
-  });
-  
+  },
+  profilePic: {
+      type: String,
+      required: true
+  },
+  phoneExt: {
+      type: String,
+      required: true
+  },
+  dni: {
+      type: String,
+      required: true
+  },
+  position: {
+      type: String,
+      required: function() {
+          return this.profileType !== "HR";
+      }
+  },
+  departmentId: {
+      type: Schema.Types.ObjectId,
+      required: function() {
+          return this.profileType !== "HR";
+      }
+  },
+  bankAccount: {
+      type: String,
+      required: function() {
+          return this.profileType !== "HR";
+      }
+  },
+  fullName: {
+      type: String,
+      required: function() {
+          return this.profileType !== "HR";
+      }
+  },
+  studies: {
+      type: String,
+      required: function() {
+          return this.profileType !== "HR";
+      }
+  },
+  birthDate: {
+      type: Date,
+      required: function() {
+          return this.profileType !== "HR";
+      }
+  },
+  address: {
+      type: String,
+      required: function() {
+          return this.profileType !== "HR";
+      }
+  },
+  phoneNumber: {
+      type: Number,
+      required: function() {
+          return this.profileType !== "HR";
+      }
+  },
+  personalMail: {
+      type: String,
+      required: function() {
+          return this.profileType !== "HR";
+      }
+  },
+  status: {
+      type: String,
+      enum: ["Sick leave", "Working", "Former"],
+      required: function() {
+          return this.profileType !== "HR";
+      }
+  },
+  removedAt: Date
+});
   const userModel = mongoose.model("userModel", userSchema);
    
   module.exports = userModel
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,12 @@
 const { Router } = require('express')
-const { getUser, addUser } = require('../controllers/userController')
+const { getUser, addUser, resetToDefault } = require('../controllers/userController')
 
 const userRouter = Router()
 
 userRouter.get("/", getUser)
 userRouter.post("/", addUser)
+
+// Para tener la base de datos siempre con los mismos datos y poder hacer test de eliminar
+userRouter.get("/resetToDefault", resetToDefault)
 
 module.exports = { userRouter }
