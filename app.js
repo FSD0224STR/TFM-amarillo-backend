68b66d608d67ffada3daac45620974d57e819cd5
Bego Montes <begomontes9498@gmail.com>
2024-05-17 20:13:44 +0200

    Add: Absence codes model

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ const { taskRouter } = require("./routes/taskRoutes");
 const { goalRouter } = require("./routes/goalRoutes");
 const { departmentRouter } = require("./routes/departmentRoutes");
 const { companyRouter } = require("./routes/companyRoutes");
+const { absenceCodeRouter } = require("./routes/absenceCodeRoutes");
 
 app.use('/users', userRouter)
 app.use('/absences', absenceRouter)
@@ -35,6 +36,7 @@ app.use('/tasks', taskRouter)
 app.use('/goals', goalRouter)
 app.use('/departments', departmentRouter)
 app.use('/companies', companyRouter)
+app.use('/absenceCodes', absenceCodeRouter)
 
 const server = app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
