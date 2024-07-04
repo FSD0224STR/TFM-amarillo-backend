f3f83ecf82c2795b18ee082e4c4443f862fddc43
Angela Toral <158821520+AngToral@users.noreply.github.com>
2024-07-04 17:24:37 +0200

    minor changes

--- models/expense.model.js
+++ models/expense.model.js
@@ -1,44 +1,41 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const expenseSchema = new Schema({
-    absenceId: {
-        type: Schema.Types.ObjectId,
-        ref: 'absenceModel',
-        required: true,
+const expenseSchema = new Schema(
+    {
+        absenceId: {
+            type: Schema.Types.ObjectId,
+            ref: "absenceModel",
+            required: true,
+        },
+        paymentMethod: {
+            enum: ["Personal", "Business Card"],
+            type: String,
+            required: true,
+        },
+        creditCardEnd: {
+            type: Number,
+        },
+        expenseCodeId: {
+            type: Array,
+            required: true,
+        },
+        expenseStatus: {
+            type: String,
+            enum: ["Pendiente", "Aprobado"],
+            required: true,
+        },
+        expensePayment: {
+            type: Date,
+        },
+        expenseProof: {
+            type: Array,
+        },
+        removedAt: Date,
     },
-    // expenseTitle: {
-    //     type: String,
-    //     required: true
-    // },
-    paymentMethod: {
-        enum: ["Personal", "Business Card"],
-        type: String,
-        required: true
-    },
-    creditCardEnd: {
-        type: Number,
-    },
-    expenseCodeId: {
-        type: Array,
-        required: true,
-    },
-    expenseStatus: {
-        type: String,
-        enum: ["Pendiente", "Aprobado"],
-        required: true
-    },
-    expensePayment: {
-        type: Date,
-    },
-    expenseProof: {
-        type: Array,
-    },
-    removedAt: Date,
-    },
-    {timestamps: true}
+    { timestamps: true }
 );
 
 const expenseModel = mongoose.model("expenseModel", expenseSchema);
-    
-module.exports = expenseModel
+
+module.exports = expenseModel;
